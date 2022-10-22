601806c8d7a057dd0c7bb89ad8ed9384b0d84d32
Daniel Ik <codingeinstein@gmail.com>
2022-10-22 12:59:58 +0100

    update transforms

--- transforms.py
+++ transforms.py
@@ -8,18 +8,19 @@ data_config = config['data_config']
 
 mean = data_config['mean']
 std = data_config['std']
+img_size = data_config['img_size']
 
 
 train_t = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize(mean=mean, std=std),
+    transforms.Resize(img_size),
     transforms.RandomHorizontalFlip(.4),
     transforms.RandomVerticalFlip(.4),
-
-
+    transforms.ToTensor(),
+    transforms.Normalize(mean=mean, std=std),
 ])
 
 test_t = transforms.Compose([
+    transforms.Resize(img_size),
     transforms.ToTensor(),
     transforms.Normalize(mean=mean, std=std),
 ])
