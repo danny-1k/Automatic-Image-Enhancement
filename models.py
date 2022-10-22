8234a8301ee6bbce411f25b63121ba7dabae8db7
Daniel Ik <codingeinstein@gmail.com>
2022-10-22 21:04:11 +0100

    models & train script

---
+++ models.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn 
+
+
+class NetBase(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+    def forward(self, img, label):
+        
+        x_img = self.forward_image(img)
+        x_label = self.forward_label(label)
+        joined = self.join_image_label(x_img, x_label)
+        out = self.regression(joined)
+
+        return out
+
+
+    def forward_image(self, x):
+        pass
+
+    def forward_label(self, x):
+        pass
+
+    def join_image_label(self, intermidiery, label):
+        pass
+
+    def regression(self, x):
+        pass
+
+
+
+class AutoCorrectorBaseLine(NetBase):
+    def __init__(self, num_labels=1):
+        super().__init__()
+
+        self.conv_layers = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=32, kernel_size=7),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=7),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=7),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=7),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+
+        self.label_embed = nn.Embedding(num_embeddings=num_labels, embedding_dim=50)
+
+        self.pre_regression_layers = nn.Sequential(
+            nn.Linear(128*26*26, 1024),
+            nn.ReLU()
+        )
+
+        self.reg_layers = nn.Sequential(
+            nn.Linear(1024 + 50, 512),
+            nn.ReLU(),
+            nn.Linear(512, 4)      
+        )
+
+
+    def forward_image(self, x):
+        x = self.conv_layers(x)
+        x = x.view(x.shape[0], -1)
+        x = self.pre_regression_layers(x)
+        return x
+
+
+    def forward_label(self, label):
+        return self.label_embed(label)
+
+
+    def join_image_label(self, inter, label):
+        return torch.cat([inter, label], axis=1)
+
+
+    def regression(self, x):
+        return self.reg_layers(x)
+
+
+
+
+def fetch_model(name):
+    name = name.lower()
+    models = {
+        'baseline': AutoCorrectorBaseLine,
+    }
+
+    for model_name in list(models.keys()):
+        if model_name in name:
+            return models[model_name]
+
+    
+    raise ValueError(f'`{name}` is not a model.\n\t Available -> {list(models.keys())}')
+
+
+if __name__ == '__main__':
+    # x = torch.zeros((1, 3, 256, 256))
+
+    # n = AutoCorrectorBaseLine()
+
+    # print(n(x, torch.Tensor([0]).long()))
+
+    print(fetch_model('autosomthing baseline'))
---
+++ train.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torch.optim import Adam
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import yaml
+
+from tqdm import tqdm
+
+from data import ImageData
+
+import argparse
+
+from models import fetch_model
+
+
+def train(model_name, device, train_config, test_config):
+
+
+    traindata = ImageData(train=True)
+    testdata = ImageData(train=False)
+
+    train = DataLoader(traindata, batch_size=train_config['batch_size'], shuffle=True)
+    test = DataLoader(testdata, batch_size=test_config['batch_size'], shuffle=True)
+
+    net = fetch_model(model_name)(num_labels=len(testdata.labels))
+
+    net.to(device)
+
+    try:
+        train_config = train_config['models'][model_name]
+    except:
+        raise ValueError(f'Configuration for model `{model_name}` does not exist')
+
+    optimizer = Adam(net.parameters(), lr=train_config['lr'])
+    scheduler = ReduceLROnPlateau(optimizer)
+
+    lossfn = nn.MSELoss()
+
+    for epoch in tqdm(range(train_config['epochs'])):
+
+        net.train()
+
+        for x, label, y in train:
+            
+            x = x.to(device)
+            label = label.to(device).long()
+            y = y.to(device)
+
+            p = net(x, label)
+            loss = lossfn(p, y)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step(loss)
+
+        net.eval()
+        with torch.no_grad():
+            for x, label, y in test:
+
+                x = x.to(device)
+                label = label.to(device).long()
+                y = y.to(device)
+
+                p = net(x, label)
+                loss = lossfn(p, y)
+
+
+        print(f'Epoch : {epoch + 1} test_loss : {loss.item()}')
+
+
+
+
+if __name__ == '__main__':
+    config = yaml.load(open('config.yml', 'r').read(), Loader=yaml.Loader)
+
+    trainconfig = config['train_parameters']
+    testconfig = config['test_parameters']
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    parser = argparse.ArgumentParser(description='Train script')
+    parser.add_argument('--model_name', required=True, help='Model name. Should be in the config.yml')
+    args = parser.parse_args()
+
+    model_name = args.model_name
+
+    train(model_name=model_name, train_config=trainconfig, test_config=testconfig, device=device)
