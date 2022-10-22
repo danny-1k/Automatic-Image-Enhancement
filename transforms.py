2b7c02c0388aa13519ee2347cfacb290212897e6
Daniel Ik <codingeinstein@gmail.com>
2022-10-22 12:00:57 +0100

    update transforms

--- transforms.py
+++ transforms.py
@@ -1,9 +1,18 @@
 import torch
 import torchvision.transforms as transforms
+import yaml
+
+config = yaml.load(open('config.yml', 'r').read(), Loader=yaml.Loader)
+
+data_config = config['data_config']
+
+mean = data_config['mean']
+std = data_config['std']
+
 
 train_t = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize(mean=(), std=()),
+    transforms.Normalize(mean=mean, std=std),
     transforms.RandomHorizontalFlip(.4),
     transforms.RandomVerticalFlip(.4),
 
@@ -12,5 +21,5 @@ train_t = transforms.Compose([
 
 test_t = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize(mean=(), std=()),
+    transforms.Normalize(mean=mean, std=std),
 ])
