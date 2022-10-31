d0aeb79a2c7846f28f46b1820dab6f6a7b86bf35
Daniel Ik <codingeinstein@gmail.com>
2022-10-31 22:01:31 +0100

    update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -1,6 +1,6 @@
 from PIL import Image, ImageEnhance
 import numpy as np
-
+from constants import *
 
 def read_img(img):
     img = Image.open(img)
@@ -32,16 +32,35 @@ def adjust_sharpness(img, value):
     return img, value
 
 
-def random_manipulation(img, min=-2, max=2):
+def random_manipulation(img):
 
-    brightness_valaue = np.random.uniform(min, max)
-    saturation_value = np.random.uniform(min, max)
-    contrast_value = np.random.uniform(min, max)
-    sharpness_value = np.random.uniform(min, max)
+    # we want the model to correct images that are in the specified ranges in `CONSTANTS.py`
+    # the correction will be added to the original values of the image
+
+
+    brightness_value = np.random.uniform(*BRIGHTNESS)
+    saturation_value = np.random.uniform(*SATURATION)
+    contrast_value = np.random.uniform(*CONTRAST)
+    sharpness_value = np.random.uniform(*SHARPNESS)
+
+    brightness_adjustment = brightness_value - 1
+    saturation_adjustment = saturation_value - 1
+    contrast_adjustment = contrast_value - 1
+    sharpness_adjustment = sharpness_value - 1
+    
 
     img, _ = adjust_brightness(img, brightness_valaue)
     img, _ = adjust_saturation(img, saturation_value)
     img, _ = adjust_contrast(img, contrast_value)
     img, _ = adjust_sharpness(img, sharpness_value)
 
-    return img, [brightness_valaue, saturation_value, contrast_value, sharpness_value]
+    return img, [brightness_adjustment, saturation_adjustment, contrast_adjustment, sharpness_adjustment]
+
+
+def defined_manipulation(img, brightness, saturation, contrast, sharpness):
+    img, _ = adjust_brightness(img, brightness)
+    img, _ = adjust_saturation(img, saturation)
+    img, _ = adjust_contrast(img, contrast)
+    img, _ = adjust_sharpness(img, sharpness)
+
+    return img
