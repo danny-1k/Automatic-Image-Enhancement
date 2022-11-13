822c0119e5d1f4299656dd804cfa78d5fa436684
Daniel Ik <codingeinstein@gmail.com>
2022-11-13 19:26:19 +0100

    misslabelling of data with no augmentation

--- data_utils.py
+++ data_utils.py
@@ -37,33 +37,50 @@ def random_manipulation(img):
     # we want the model to correct images that are in the specified ranges in `CONSTANTS.py`
     # the correction will be added to the original values of the image
 
+    if np.random.rand() > .5: # sometimes let it predict normal images
 
-    brightness_value = np.random.uniform(*BRIGHTNESS)
-    saturation_value = np.random.uniform(*SATURATION)
-    contrast_value = np.random.uniform(*CONTRAST)
-    sharpness_value = np.random.uniform(*SHARPNESS)
+        brightness_value = 1
+        saturation_value = 1
+        contrast_value = 1
+        sharpness_value = 1
 
-    brightness_adjustment = brightness_value - NORMAL_BRIGHTNESS
-    saturation_adjustment = saturation_value - NORMAL_SATURATION
-    contrast_adjustment = contrast_value - NORMAL_CONTRAST
-    sharpness_adjustment = sharpness_value - NORMAL_SHARPNESS
+        brightness_adjustment = brightness_value - NORMAL_BRIGHTNESS
+        saturation_adjustment =  saturation_value - NORMAL_SATURATION
+        contrast_adjustment = contrast_value - NORMAL_CONTRAST
+        sharpness_adjustment = sharpness_value - NORMAL_SHARPNESS
+        
+
+        return img, [brightness_adjustment, saturation_adjustment, contrast_adjustment, sharpness_adjustment]
 
 
-    #normalize between 0 and 1
+    else:
 
-    brightness_adjustment = (brightness_adjustment-MIN_BRIGHTNESS_LABEL)/(MAX_BRIGHTNESS_LABEL-MIN_BRIGHTNESS_LABEL)
-    saturation_adjustment = (saturation_adjustment-MIN_SATURATION_LABEL)/(MAX_SATURATION_LABEL-MIN_SATURATION_LABEL)
-    contrast_adjustment = (contrast_adjustment-MIN_CONTRAST_LABEL)/(MAX_CONTRAST_LABEL-MIN_CONTRAST_LABEL)
-    sharpness_adjustment = (sharpness_adjustment-MIN_SHARPNESS_LABEL)/(MAX_SHARPNESS_LABEL-MIN_SHARPNESS_LABEL)
+        brightness_value = np.random.uniform(*BRIGHTNESS)
+        saturation_value = np.random.uniform(*SATURATION)
+        contrast_value = np.random.uniform(*CONTRAST)
+        sharpness_value = np.random.uniform(*SHARPNESS)
 
-    
+        brightness_adjustment = brightness_value - NORMAL_BRIGHTNESS
+        saturation_adjustment = saturation_value - NORMAL_SATURATION
+        contrast_adjustment = contrast_value - NORMAL_CONTRAST
+        sharpness_adjustment = sharpness_value - NORMAL_SHARPNESS
 
-    img, _ = adjust_brightness(img, brightness_value)
-    img, _ = adjust_saturation(img, saturation_value)
-    img, _ = adjust_contrast(img, contrast_value)
-    img, _ = adjust_sharpness(img, sharpness_value)
 
-    return img, [brightness_adjustment, saturation_adjustment, contrast_adjustment, sharpness_adjustment]
+        #normalize between 0 and 1
+
+        brightness_adjustment = (brightness_adjustment-MIN_BRIGHTNESS_LABEL)/(MAX_BRIGHTNESS_LABEL-MIN_BRIGHTNESS_LABEL)
+        saturation_adjustment = (saturation_adjustment-MIN_SATURATION_LABEL)/(MAX_SATURATION_LABEL-MIN_SATURATION_LABEL)
+        contrast_adjustment = (contrast_adjustment-MIN_CONTRAST_LABEL)/(MAX_CONTRAST_LABEL-MIN_CONTRAST_LABEL)
+        sharpness_adjustment = (sharpness_adjustment-MIN_SHARPNESS_LABEL)/(MAX_SHARPNESS_LABEL-MIN_SHARPNESS_LABEL)
+
+        
+
+        img, _ = adjust_brightness(img, brightness_value)
+        img, _ = adjust_saturation(img, saturation_value)
+        img, _ = adjust_contrast(img, contrast_value)
+        img, _ = adjust_sharpness(img, sharpness_value)
+
+        return img, [brightness_adjustment, saturation_adjustment, contrast_adjustment, sharpness_adjustment]
 
 
 def defined_manipulation(img, brightness, saturation, contrast, sharpness):
@@ -73,3 +90,12 @@ def defined_manipulation(img, brightness, saturation, contrast, sharpness):
     img, _ = adjust_sharpness(img, sharpness)
 
     return img
+
+
+def unnormalize(brightness, saturation, contrast, sharpness):
+    brightness = brightness*(MAX_BRIGHTNESS_LABEL-MIN_BRIGHTNESS_LABEL) + MIN_BRIGHTNESS_LABEL
+    saturation = saturation*(MAX_SATURATION_LABEL-MIN_SATURATION_LABEL) + MIN_SATURATION_LABEL
+    contrast = contrast*(MAX_CONTRAST_LABEL-MIN_CONTRAST_LABEL) + MIN_CONTRAST_LABEL
+    sharpness = sharpness*(MAX_SHARPNESS_LABEL-MIN_SHARPNESS_LABEL) + MIN_SHARPNESS_LABEL
+
+    return brightness, saturation, contrast, sharpness
