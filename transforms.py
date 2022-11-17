ce001b0a6210005f1cb5fe0fa511ec7d59c55178
Daniel Ik <codingeinstein@gmail.com>
2022-11-17 08:04:02 +0100

    vgg transforms

--- transforms.py
+++ transforms.py
@@ -18,6 +18,17 @@ vgg_transform = transforms.Compose([
     transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
 ])
 
+vgg_train_transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.RandomHorizontalFlip(.4),
+    transforms.RandomVerticalFlip(.4),
+    transforms.RandomAffine(45),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+
+])
+
 
 train_t = transforms.Compose([
     transforms.Resize(256),
