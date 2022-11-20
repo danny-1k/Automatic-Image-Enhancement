d3694d981fe6507c12a1749e463a818446d2bce6
Daniel Ik <codingeinstein@gmail.com>
2022-11-20 21:20:40 +0100

    fix bug

--- models.py
+++ models.py
@@ -285,15 +285,19 @@ class MapModel(nn.Module):
         )
         self.E4 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
 
         )
         self.E5 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
 
         )
@@ -301,32 +305,42 @@ class MapModel(nn.Module):
 
         self.D1 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
             nn.ReLU(),
         )
         self.D2 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
         )
         self.D3 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
         )
         self.D4 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.Conv2d(in_channels=64, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
         )
         self.D5 = nn.Sequential(
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Conv2d(in_channels=32, out_channels=output_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(output_channels),
             nn.ReLU(),
         )
 
@@ -380,6 +394,146 @@ class MapModel(nn.Module):
 
         return x
 
+class MapModelPretrainedVGG(nn.Module):
+    def __init__(self, output_channels=2):
+        super().__init__()
+        feature_extractor = vgg16(pretrained=True).eval().requires_grad_(False).features
+
+        self.encoder_maxpool = nn.MaxPool2d(2,2, return_indices=True) # fuck vgg maxpool
+        self.decoder_maxpool = nn.MaxUnpool2d(2,2)
+
+        #Pretrained encoder
+
+        self.E1 = nn.Sequential(
+            feature_extractor[0],
+            feature_extractor[1],
+            feature_extractor[2],
+            feature_extractor[3],
+        ).eval().requires_grad_(False)
+
+        self.E2 = nn.Sequential(
+            feature_extractor[5],
+            feature_extractor[6],
+            feature_extractor[7],
+            feature_extractor[8],
+        ).eval().requires_grad_(False)
+
+        self.E3 = nn.Sequential(
+            feature_extractor[10],
+            feature_extractor[11],
+            feature_extractor[12],
+            feature_extractor[13],
+            feature_extractor[14],
+            feature_extractor[15],
+        ).eval().requires_grad_(False)
+
+        self.E4 = nn.Sequential(
+            feature_extractor[17],
+            feature_extractor[18],
+            feature_extractor[19],
+            feature_extractor[20],
+            feature_extractor[21],
+            feature_extractor[22],
+        ).eval().requires_grad_(False)
+
+        self.E5 = nn.Sequential(
+            feature_extractor[24],
+            feature_extractor[25],
+            feature_extractor[26],
+            feature_extractor[27],
+            feature_extractor[28],
+            feature_extractor[29],
+        ).eval().requires_grad_(False)
+
+
+
+        # Decoder does not have to be symetric
+        # because the encoder net uses same convolutions to keep the
+        # spatial dimensions the same througout the block until a maxpool layer
+
+
+        self.D1 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+        )
+        self.D2 = nn.Sequential(
+            nn.Conv2d(in_channels=512, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+        )
+        self.D3 = nn.Sequential(
+            nn.Conv2d(in_channels=256, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
+        self.D4 = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+        )
+        self.D5 = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=output_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(output_channels),
+            nn.ReLU(),
+        )
+
+
+
+    def forward(self, x):
+        output1 = x.size()
+        x = self.E1(x)
+        x, ind1 = self.encoder_maxpool(x)
+
+        output2 = x.size()
+        x = self.E2(x)
+        x, ind2 = self.encoder_maxpool(x)
+
+        output3 = x.size()
+        x = self.E3(x)
+        x, ind3 = self.encoder_maxpool(x)
+
+        output4 = x.size()
+        x = self.E4(x)
+        x, ind4 = self.encoder_maxpool(x)
+
+        output5 = x.size()
+        x = self.E5(x)
+        x, ind5 = self.encoder_maxpool(x)
+
+
+        x = self.decoder_maxpool(x, indices=ind5, output_size=output5)
+        x = self.D1(x)
+
+        x = self.decoder_maxpool(x, indices=ind4, output_size=output4)
+        x = self.D2(x)
+        
+        x = self.decoder_maxpool(x, indices=ind3, output_size=output3)
+        x = self.D3(x)
+        
+        x = self.decoder_maxpool(x, indices=ind2, output_size=output2)
+        x = self.D4(x)
+        
+        x = self.decoder_maxpool(x, indices=ind1, output_size=output1)
+        x = self.D5(x)
+
+
+        return x
 
 
 def fetch_model(name):
@@ -391,8 +545,7 @@ def fetch_model(name):
         'smallnet': SmallNet,
         'mapmodel': MapModel,
         'map': MapModel,
-
-
+        'mapvgg':MapModelPretrainedVGG
     }
 
     for model_name in list(models.keys()):
